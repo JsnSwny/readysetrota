bae2dc428ab6cefae27be6bdf1005e922ca60218
Jason Sweeney <jsnswny@gmail.com>
2020-04-13 12:52:00 +0100

    Attemp to fix main.js in build

--- rotaready/settings.py
+++ rotaready/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'rotaready.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'build')],
+        'DIRS': [os.path.join(BASE_DIR, '/static/frontend')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
