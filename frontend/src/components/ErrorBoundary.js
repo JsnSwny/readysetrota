94c4a4afa7c6b6640ba0bd758785d31557edc1b5
Jason Sweeney <js418@hw.ac.uk>
2021-03-07 18:27:58 +0000

    Fixed forecast modal date

--- frontend/src/components/ErrorBoundary.js
+++ frontend/src/components/ErrorBoundary.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class ErrorBoundary extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { error: '',
-        errorInfo: '',
-        hasError: false, };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { error: "", errorInfo: "", hasError: false };
+  }
   static getDerivedStateFromError(error) {
     return { hasError: true, error };
   }
@@ -19,30 +17,28 @@ export default class ErrorBoundary extends Component {
     if (hasError) {
       return (
         <div className="error-container container-2">
-        <h1>
-            There was an error in loading this page.{' '}
-        </h1>
-        <p>
-        <span
-            style={{ cursor: 'pointer', color: '#0077FF' }}
-            onClick={() => {
+          <h1>There was an error in loading this page. </h1>
+          <p>
+            <span
+              style={{ cursor: "pointer", color: "#0077FF" }}
+              onClick={() => {
                 window.location.reload();
-            }}
+              }}
             >
-            Reload this page
-            </span>{' '}
-        </p>
-        <details className="error-details">
+              Reload this page
+            </span>{" "}
+          </p>
+          <details className="error-details">
             <summary>Click for error details</summary>
             <h5>{error.message}</h5>
             <small>{errorInfo && errorInfo.componentStack.toString()}</small>
-        </details>
+          </details>
         </div>
       );
     }
-  return this.props.children;
+    return this.props.children;
   }
 }
 ErrorBoundary.propTypes = {
-  children: PropTypes.oneOfType([ PropTypes.object, PropTypes.array ]).isRequired,
+  children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
 };
