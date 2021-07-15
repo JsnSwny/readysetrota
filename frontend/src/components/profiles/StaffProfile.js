292a9a70dbdc118e2a2af825624e51d1f954803f
Jason <jsnswny@gmail.com>
2021-07-15 14:01:12 +0100

    Began working on staff dashboard

--- frontend/src/components/profiles/StaffProfile.js
+++ frontend/src/components/profiles/StaffProfile.js
@@ -7,7 +7,8 @@ import Availability from "./Availability";
 import HolidayRequest from "./dashboard/HolidayRequest";
 import UpcomingShifts from "./UpcomingShifts";
 import Stats from "./dashboard/stats/Stats";
-import { format } from "date-fns";
+import { format, differenceInCalendarDays, parseISO, isToday } from "date-fns";
+import Title from "../common/Title";
 
 const StaffProfile = (props) => {
   const dispatch = useDispatch();
@@ -31,6 +32,7 @@ const StaffProfile = (props) => {
 
   let openShifts = useSelector((state) => state.shifts.open_shifts);
   let shifts = useSelector((state) => state.shifts.shifts);
+  let isLoading = useSelector((state) => state.shifts.isLoading);
 
   useEffect(() => {
     dispatch(
@@ -65,45 +67,94 @@ const StaffProfile = (props) => {
 
   return (
     <Fragment>
-      <Stats
-        title="Staff Dashboard"
-        type={id_param ? "staff_profile" : "staff"}
-        employee={id_param ? employee_id : false}
-      />
-      <div className="dashboard container-2">
-        {current.site.id != 0 && (
+      <div className="banner">
+        <div className="wrapper--md flex-container--between-start">
+          <h1 className="header">
+            <Title
+              name="Welcome Back, Jason"
+              subtitle="Staff Dashboard"
+              breakWord={false}
+            />
+          </h1>
+        </div>
+      </div>
+      <div className="dashboard wrapper--md">
+        <div className="dashboard__header">
+          <h2 className="dashboard__header-title">Your Shifts</h2>
+        </div>
+        <hr class="separator" />
+        {isLoading ? (
+          <div class="dot-pulse"></div>
+        ) : shifts.length > 0 ? (
           <Fragment>
-            {current.department != 0 && currentEmployee && (
-              <Fragment>
-                <UpcomingShifts
-                  title="Open Shifts"
-                  shifts={openShifts}
-                  employee={currentEmployee}
-                  admin={siteAdmin && id_param}
-                />
-                <UpcomingShifts
-                  allow_export={true}
-                  title="Upcoming Shifts"
-                  shifts={shifts}
-                  employee={currentEmployee}
-                  admin={siteAdmin && id_param}
-                />
-                <div className="flex-container--between-start mobile">
-                  {plan != "F" && <Availability employee={currentEmployee} />}
-                  <HolidayRequest
-                    holidays={
-                      siteAdmin && id_param
-                        ? holidays.filter(
-                            (item) => item.employee.id == id_param
-                          )
-                        : holidays
-                    }
-                    admin={false}
-                  />
+            <p>
+              Your next shift is{" "}
+              {shifts[0].date == format(new Date(), "yyyy-MM-dd") ? (
+                <strong>today</strong>
+              ) : (
+                <Fragment>
+                  in{" "}
+                  <strong>
+                    {differenceInCalendarDays(
+                      parseISO(shifts[0].date),
+                      new Date()
+                    )}{" "}
+                    days
+                  </strong>
+                </Fragment>
+              )}
+            </p>
+            <div className="userShifts">
+              <div className="userShifts__today flex-container--between">
+                <h4 className="userShifts__today-date">
+                  {format(parseISO(shifts[0].date), "iiii do MMMM")}
+                </h4>
+                <div className="userShifts__today-time">
+                  <h4>
+                    {shifts[0].start_time} - {shifts[0].end_time}
+                  </h4>
+                  {/* <p>in 30 Minutes</p> */}
                 </div>
-              </Fragment>
-            )}
+              </div>
+            </div>
+            {/* <p className="title--wide">Upcoming Shifts</p> */}
+
+            <div className="dashboardShiftList">
+              <ul className="dashboardShiftList__header dashboardShiftList__list">
+                <li>Date</li>
+                <li>Time</li>
+                <li>Break (mins)</li>
+                <li>Total Length (hrs)</li>
+              </ul>
+              <div className="dashboardShiftList__shiftsAndDate">
+                <div className="dashboardShiftList__shifts">
+                  {shifts.map((item) => (
+                    <ul className="dashboardShiftList__item dashboardShiftList__list">
+                      <li
+                        className={`${
+                          isToday(parseISO(item.date)) ? "today" : ""
+                        }`}
+                      >
+                        {format(parseISO(item.date), "ccc dd MMM")}
+                      </li>
+                      <li>
+                        {item.start_time} - {item.end_time}
+                      </li>
+                      <li className="sm">
+                        {item.break_length == 0
+                          ? "N/A"
+                          : `${item.break_length}`}
+                      </li>
+                      <li className="sm">{item.length}</li>
+                    </ul>
+                  ))}
+                </div>
+                <div className="dashboardShiftList__datePicker"></div>
+              </div>
+            </div>
           </Fragment>
+        ) : (
+          <p>You have no upcoming shifts</p>
         )}
       </div>
     </Fragment>
