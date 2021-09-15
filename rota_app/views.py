80e8a289490c8bcb0978ca0bd1e20eedcb45eded
Jason <jsnswny@gmail.com>
2021-09-15 20:16:39 +0100

    Uncommented emailing

--- rota_app/views.py
+++ rota_app/views.py
@@ -118,27 +118,27 @@ class Publish(APIView):
 
         # print(all_shifts)
 
-        # connection = mail.get_connection()
-        # shifts_sorted = sorted(all_shifts, key=attrgetter("employee.id"))
-        # shifts_unique = [list(grp) for k, grp in groupby(
-        #     shifts_sorted, attrgetter("employee.id"))]
-        # email = []
-        # today_date = datetime.now().strftime("%d/%m/%Y")
-        # connection.open()
-        # for idx, val in enumerate(shifts_unique):
-        #     employee = shifts_unique[idx][0].employee
+        connection = mail.get_connection()
+        shifts_sorted = sorted(all_shifts, key=attrgetter("employee.id"))
+        shifts_unique = [list(grp) for k, grp in groupby(
+            shifts_sorted, attrgetter("employee.id"))]
+        email = []
+        today_date = datetime.now().strftime("%d/%m/%Y")
+        connection.open()
+        for idx, val in enumerate(shifts_unique):
+            employee = shifts_unique[idx][0].employee
 
-        #     if employee.user:
-        #         shifts = Shift.objects.filter(
-        #             employee__user__id=employee.user.id, stage="Published", date__gte=datetime.now()).order_by('date')
-        #         html_message = render_to_string("emailshifts.html", context={
-        #                                         'shifts': shifts, 'employee': employee})
-        #         mail_item = mail.EmailMultiAlternatives(
-        #             "Rota Updated - " + today_date, "", "readysetrota@gmail.com", [employee.user.email])
-        #         mail_item.attach_alternative(html_message, "text/html")
-        #         email.append(mail_item)
-        # connection.send_messages(email)
-        # connection.close()
+            if employee.user:
+                shifts = Shift.objects.filter(
+                    employee__user__id=employee.user.id, stage="Published", date__gte=datetime.now()).order_by('date')
+                html_message = render_to_string("emailshifts.html", context={
+                                                'shifts': shifts, 'employee': employee})
+                mail_item = mail.EmailMultiAlternatives(
+                    "Rota Updated - " + today_date, "", "readysetrota@gmail.com", [employee.user.email])
+                mail_item.attach_alternative(html_message, "text/html")
+                email.append(mail_item)
+        connection.send_messages(email)
+        connection.close()
 
         # publish_email.delay(shifts_list)
 
