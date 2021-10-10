bd3616cef88a57fbab14e5cce4edd2bbfa9886ce
Jason <jsnswny@gmail.com>
2021-10-10 18:13:35 +0100

    Reverted modal changes for main branch

--- frontend/src/components/modals/CreateShift.js
+++ frontend/src/components/modals/CreateShift.js
@@ -68,6 +68,10 @@ const CreateShift = (props) => {
   return (
     open && (
       <div className={`modal App ${open ? "open" : ""}`}>
+        <div className={`form__image ${open ? "open" : ""}`}>
+          <i className={`${imageTypes[type]}`}></i>
+        </div>
+
         <div
           className={`modal__container ${open ? "open" : ""} ${
             type == "register" ? "modal__container--sm" : ""
--- frontend/src/components/modals/employee/tabs/Permissions.js
+++ frontend/src/components/modals/employee/tabs/Permissions.js
@@ -55,42 +55,6 @@ const Permissions = ({ permissions, setPermissions }) => {
         {...permProps}
         indent={true}
       />
-      <PermissionItem
-        perm="view_forecasts"
-        permText="View Forecasts"
-        {...permProps}
-        indent={true}
-      />
-      <PermissionItem
-        perm="view_forecasts"
-        permText="View Forecasts"
-        {...permProps}
-        indent={true}
-      />
-      <PermissionItem
-        perm="view_forecasts"
-        permText="View Forecasts"
-        {...permProps}
-        indent={true}
-      />
-      <PermissionItem
-        perm="view_forecasts"
-        permText="View Forecasts"
-        {...permProps}
-        indent={true}
-      />
-      <PermissionItem
-        perm="view_forecasts"
-        permText="View Forecasts"
-        {...permProps}
-        indent={true}
-      />
-      <PermissionItem
-        perm="view_forecasts"
-        permText="View Forecasts"
-        {...permProps}
-        indent={true}
-      />
       <h3>Availabilities</h3>
       <PermissionItem
         perm="manage_availabilities"
