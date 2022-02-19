ea7d1fb4f62fff65350fdc1ed9f303088b394faf
JsnSwny <jsnswny@gmail.com>
2022-02-19 16:54:22 +0000

    Import Fragment

--- frontend/src/components/management/tables/Employees.js
+++ frontend/src/components/management/tables/Employees.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   getAllEmployees,
